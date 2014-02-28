7c418077118583e727c6376d0546e2dd6f16e88f
Ovidiu Miron <omiron1@yahoo.com>
2014-02-28 11:11:35 +0200

    worked for compatible zope with cmd line

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
-import os.path
 import datetime
+import logging
+import os.path
+
+from sims.checklistgenerator import ChecklistGenerator
+from sims.configloader import ConfigLoader
+from sims.reportgenerator import ReportGenerator
+
+logger = logging.getLogger('utils')
 
 
 def generate_file_name(folderName, CountryCode, xmlFileNameSpecies):
@@ -21,3 +28,23 @@ def getValueFromGeneric(record, item):
         value = getattr(record, item)
 
     return str(value) if value is not None else ""
+
+
+def make_report(args):
+    configLoader = ConfigLoader(args.action, args.type)
+
+    fileNameExport = generate_file_name(
+        args.xml_path, configLoader.country, "_{0}".format(
+            configLoader.file_name))
+
+    with open(fileNameExport, "w") as xml_file:
+        if args.action == "report":
+            action = ReportGenerator(args.type, configLoader)
+        elif args.action == "checklist":
+            action = ChecklistGenerator(args.type, configLoader)
+        else:
+            assert False
+        xml_string = action()
+        xml_file.write(xml_string)
+
+    logger.info("Generated filename: {0} with success".format(fileNameExport))
