9667fc300d5180f6f8dd8cc19bac6be8913e7c70
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2014-04-29 11:19:30 +0300

    cleanup code

--- sims/xmlgenerator.py
+++ sims/xmlgenerator.py
@@ -43,16 +43,14 @@ def getCountryISOCode(countryCode):
 
 class XMLGenerator(object):
     def __init__(self, xml_root_tag, xml_schema_species):
-        NS = 'http://www.w3.org/2001/XMLSchema-instance'
-        location_attribute = '{%s}noNameSpaceSchemaLocation' % NS
-
+        location_attribute = '{http://www.w3.org/2001/XMLSchema-instance}noNameSpaceSchemaLocation'
         self.export_xml = etree.Element(
-            xml_root_tag, attrib={location_attribute: xml_schema_species})
-        """
-        set the language
-        """
-        self.export_xml.attrib['{http://www.w3.org/XML/1998/namespace}lang'] =\
-            "en"
+                            xml_root_tag,
+                            attrib={
+                                location_attribute: xml_schema_species
+                            }
+                        )
+        self.export_xml.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "en"
 
     def append(self, node):
         self.export_xml.append(node)
